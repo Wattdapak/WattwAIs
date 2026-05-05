456bf3551cbc68592ee2ab4113ec353463a9bb40
hungrychef-bytescode <hungrychefbytescode@gmail.com>
2026-05-05 13:54:18 +0800

    feat: define route constants for navigation

---
+++ lib/core/routes/screen_routes.dart
@@ -0,0 +1,14 @@
+//route constants
+
+class Routes {
+
+  static const dashboard = '/dashboard';
+  static const splashscreen = '/splashscreen';
+  // static const billhistory = '/bill-history';
+  // static const appliance = '/appliance';
+  // static const analytics = '/analytics';
+  // static const prediction = '/prediction';
+  // static const settings = '/settings';
+  // static const loading = '/loading';
+
+}
