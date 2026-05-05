611da6042fceeb290ecf9d544ada8056a63bd889
hungrychef-bytescode <hungrychefbytescode@gmail.com>
2026-05-05 13:54:57 +0800

    feat: configure GoRouter with splashscreen and dashboard routes

---
+++ lib/core/routes/router.dart
@@ -0,0 +1,23 @@
+import "package:go_router/go_router.dart";
+import "package:wattwais/core/routes/screen_routes.dart";
+
+import "package:wattwais/screens/dashboard.dart";
+import "package:wattwais/screens/splash_screen.dart";
+
+class WattwaisRouter {
+  static final router = GoRouter(
+    initialLocation: Routes.splashscreen,
+
+    routes: [
+      GoRoute(
+        path: Routes.splashscreen,
+        builder: (context, state) => const LogoScreen(),
+      ),
+
+      GoRoute(
+        path: Routes.dashboard,
+        builder: (context, state) => Dashboard(),
+      )
+    ],
+  );  
+}
