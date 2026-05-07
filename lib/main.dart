1b45317fe66fa46507ea5ae57f6c6dea639d9118
hungrychef-bytescode <hungrychefbytescode@gmail.com>
2026-05-07 23:38:13 +0800

    feat: setup Firebase configuration for Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 import "package:wattwais/wattwais.dart";
+import "package:firebase_core/firebase_core.dart";
+import "firebase_options.dart";
 
 /*
 App entry point for the WattwAIs app
@@ -7,4 +9,12 @@ WattwAIs - an electricity consumption monitoring and prediction app
 built using Flutter, Dart, Firebase as DB and XGBoost model for ML prediction
  */
 
-void main () => runApp(WattwAIsApp());
+void main () async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+
+  runApp(WattwAIsApp());
+} 
