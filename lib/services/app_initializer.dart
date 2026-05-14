410b64271b61687f18e37d85c9520adf4426a2a6
hungrychef-bytescode <hungrychefbytescode@gmail.com>
2026-05-14 10:06:08 +0800

    chore: remove unused AppInitializer file

--- lib/services/app_initializer.dart
+++
@@ -1,9 +0,0 @@
-class AppInitializer {
-  static Future<void> initialize() async {
-    await Future.delayed(Duration(seconds: 2));
-
-    // Example:
-    // await Firebase.initializeApp();
-    // await loadUserData();
-  }
-}
